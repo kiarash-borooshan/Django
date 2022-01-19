87dc733a24055d607fc697914177041b5d935a4c
kiarash.borooshan <kiarash.borooshan@gmail.com>
2022-01-19 12:19:52 +0330

    model update

--- BedeBestoon/Bestoon/my_web/admin.py
+++ BedeBestoon/Bestoon/my_web/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Post
 # Register your models here.
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ("Title", "Amount", "Date", "Choose")
+
+@admin.register(Post)
+class Post_Admin(admin.ModelAdmin):
+    list_display = ("Title",)
--- BedeBestoon/Bestoon/my_web/models.py
+++ BedeBestoon/Bestoon/my_web/models.py
@@ -1,6 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Post(models.Model):
+    Status_Choices = (
+        ("draft", "Draft"),
+        ("publish", "Publish")
+    )
+    Title = models.CharField(
+        max_length=200
+    )
+    Slug = models.SlugField(
+        max_length=250,
+        unique_for_date="publish"
+    )
+    Author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_post"
+    )
+    Body = models.TextField()
+    publish = models.DateTimeField(
+        default=timezone.now
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    update = models.DateTimeField(
+        auto_now=True
+    )
+    Status = models.CharField(
+        max_length=10,
+        choices=Status_Choices,
+        default="draft"
+    )
+
+    # class Meta:
+    #     ordering = "-publish"
+
+    def __str__(self):
+        return self.Title
 
 
 class Expense(models.Model):
